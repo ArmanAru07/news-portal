60d708c5bd42b87bb99e41235077c5269811e8af
ArmanAru <armanhosen768@gmail.com>
2022-09-03 14:00:13 +0600

    news

--- js/Category.js
+++ js/Category.js
@@ -5,19 +5,83 @@ const loadCategorys = () =>{
 }
 
 const displayCategorys = categorys =>{
-    console.log(categorys)
+    // console.log(categorys)
     const categorysItem = document.getElementById('categorys-item');
     categorys.forEach(category => {
         const categoryButton = document.createElement('p');
         categoryButton.classList.add('category');
-        console.log(category);
+        // console.log(category);
         categoryButton.innerHTML =`
-        <a class="nav-link active" aria-current="page" href="#"  style="text-decoration: none;">${category.category_name}</a>
+        <a onclick="loadNews('${category.category_id}')" class="nav-link active " aria-current="page" href="#"  style="text-decoration: none;">${category.category_name}</a>
         `;
         categorysItem.appendChild(categoryButton);
         
     })
-
-    
 }
+const loadNews = (category_id) => {
+    
+    const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayNews(data.data))
+}
+
+const displayNews = category =>{
+    const Maindiv = document.getElementById('Main-div');
+    Maindiv.innerHTML = '';
+    category.forEach(element => {
+        console.log(element)
+        
+    
+    // const news = document.getElementById('newses');
+    const newses = document.createElement('div')
+    
+    newses.innerHTML = `
+
+    <div class="card mb-3">
+    <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${element.image_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text">${element.details}</p>
+        <div class="d-flex justify-content-between">
+          <div class="d-flex">
+              <div>
+                  <img class="img-fluid rounded-circle" height="100" width="100" src="${element.author.img}" alt="">
+              </div>
+              <div class="p-3 ">
+                  <h6>${element.author.name}</h6>
+                  <p><small>${element.author.published_date}</small></p>
+              </div>
+          </div>
+          <div class="p-3 "><i class="fa-regular fa-eye"></i><span>${element.total_view}</span></div>
+          <div class="p-3 ">
+              <span>${element.rating.number}</span>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>
+          </div>
+          <div class="p-3 ">
+          </i><button class="btn btn-outline-success href="#" " type="submit">Read More..</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+    </div>
+    
+    `
+    Maindiv.appendChild(newses);
+    });
+    
+    
+     
+}
+
+
 loadCategorys();
